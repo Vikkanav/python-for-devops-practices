e38a5f304d7703a0d6255ec2896d790ca97bdc7c
vikash <mukthavikash@gmail.com>
2023-11-06 21:18:19 +0530

    This session like Functions, Modules, Packages & Virtual Environment

---
+++ Day04/def_key.py
@@ -0,0 +1,25 @@
+num1 = 17
+num2 = 2
+
+def addition():
+    add = num1 + num2
+    print(add)
+
+def sub():
+    s = num1 -num2
+    print(s)
+
+def mul():
+    m = num1 * num2
+    print(m)
+
+def division():
+    div = num1 / num2
+    print(div)
+
+
+addition()
+sub()
+mul()
+division()
+
---
+++ Day04/exp1.py
@@ -0,0 +1,15 @@
+# In this example, def is Keyword, greet is a function that takes a name argument and returns a greeting message.
+
+def greet(name):
+    return f"Hello, {name}!"
+
+message = greet("Alice")
+print(message)
+
+
+
+def greet(name):
+    return f"HELLO, {name}!!!!"
+
+checking = greet("Akhi")
+print(checking)
